40936f05cbd962d5a04f673f18d78ea89689e00f
toddlerer <74579078+toddlerer@users.noreply.github.com>
2023-01-04 22:40:30 +0900

    Add `erg.restartLanguageServer` command

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,67 @@
-// import { LanguageClientOptions } from "vscode-languageclient";
-import { ExtensionContext, window } from "vscode";
-import { LanguageClient, ServerOptions } from "vscode-languageclient/node";
-import { spawnSync } from "child_process";
+import { ExtensionContext, commands, window } from "vscode";
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from "vscode-languageclient/node";
+import { spawn } from "child_process";
 
 let client: LanguageClient | undefined;
 
-export async function activate_els(context: ExtensionContext) {
-    let serverOptions;
-    let result = spawnSync("erg", ["--build-features"]);
-    if (result.stdout.toString().includes("els")) {
-        serverOptions = {
-            command: "erg",
-            args: ["--language-server"]
-        };
-    } else {
-        serverOptions = {
-            command: "els",
-            args: []
-        };
-    }
-    const clientOptions = {
-        documentSelector: [
-            {
-                scheme: "file",
-                language: "erg",
-            }
-        ],
-    };
-    client = new LanguageClient("els", serverOptions, clientOptions);
-    context.subscriptions.push(client.start());
+async function startLanguageClient(context: ExtensionContext) {
+	try {
+		const buildFeatures = spawn("erg", ["--build-features"]);
+		let result = "";
+		for await (const chunk of buildFeatures.stdout) {
+			console.log(chunk);
+			result += chunk;
+		}
+		let serverOptions: ServerOptions;
+		if (result.includes("els")) {
+			serverOptions = {
+				command: "erg",
+				args: ["--language-server"],
+			};
+		} else {
+			serverOptions = {
+				command: "els",
+				args: [],
+			};
+		}
+		const clientOptions: LanguageClientOptions = {
+			documentSelector: [
+				{
+					scheme: "file",
+					language: "erg",
+				},
+			],
+		};
+		client = new LanguageClient("els", serverOptions, clientOptions);
+		context.subscriptions.push(client.start());
+	} catch (e) {
+		window.showErrorMessage(
+			"Failed to start ELS (Erg Language Server). Please make sure you have erg (built with `els` feature) or els installed.",
+		);
+	}
+}
+
+async function restartLanguageClient(context: ExtensionContext) {
+	if (client === undefined) {
+		window.showErrorMessage("Failed to restart ELS (Erg Language Server).");
+		return;
+	}
+	await client.stop();
+	client.outputChannel.dispose();
+	await startLanguageClient(context);
 }
 
 export async function activate(context: ExtensionContext) {
-    try {
-        activate_els(context);
-    } catch (e) {
-        window.showErrorMessage("Failed to start ELS (Erg Language Server). Please make sure you have erg (built with `els` feature) or els installed.");
-    }
+	context.subscriptions.push(
+		commands.registerCommand("erg.restartLanguageServer", () =>
+			restartLanguageClient(context),
+		),
+	);
+	await startLanguageClient(context);
 }
 
 export function deactivate() {
